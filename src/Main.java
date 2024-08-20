0b26fbb111f41777e180054369ccc6d5f0c13526
Khalerdo <165291699+Khalerdo@users.noreply.github.com>
2024-08-20 17:39:03 -0300

    Calculadora simples

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        digitarNumeros();
+    }
+
+
+    static void digitarNumeros() {
+        System.out.println("Pequena calculadora!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int $numero = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Digite o segundo número: ");
+        int $segundoNumero = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Você quer somar ou subtrair?\n1 para somar\n2 para subtrair\n");
+        int $pergunta = Integer.parseInt(scanner.nextLine());
+
+        int $soma = $numero + $segundoNumero;
+        int $subtracao = $numero - $segundoNumero;
+
+        if ($pergunta == 1) {
+            System.out.println("Seu resultado é: " + $soma);
+        } else {
+            if ($pergunta == 2) {
+                System.out.println("Seu resultado é: " + $subtracao);
+            } else {
+                System.out.println("Opção inválida, tente novamente.\n");
+                digitarNumeros();
+            }
+        }
+    }
+}
